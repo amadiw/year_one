2a61a8931ec268d936652b516e909f97a7a1dcf1
Amadi Someswaran <amadi@someswaran.com>
2020-11-19 22:48:41 -0500

    feat: #5 search form component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,11 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Search() {
+  const [searching, setSearching] = useState(false);
+  const [message, setMessage] = useState(null);
+  const [query, setQuery] = useState("");
+  const [movie, setMovie] = useState([]);
+
+
+  const searchMovies = async (evt) => {
+
+    evt.preventDefault();
+    setSearching(true);
+    try {
+      const { data : results} = await axios.get(
+        `http://www.omdbapi.com/?apikey=4389c305&s=${query}`
+      );
+      setMessage(null);
+      setMovie(results.Search);
+      setSearching(false);
+    } catch (error) {
+      setMessage("An unexpected error occured");
+      setSearching(false);
+    }
+  };
+  console.log('movie------ ', movie)
+
   return (
     <div>
-    <h2>
-      SEARCH
-    </h2>
+      <h2>SEARCH</h2>
+      <form onSubmit={searchMovies}>
+        <input type="text" name="query" placeholder="enter movie title"
+        value={query} onChange={(evt) => {setQuery(evt.target.value)}}/>
+        <button type="submit">X</button>
+      </form>
+      <div>
+        {movie.map(elem => (
+          <div key={elem.imdbID}>
+          <div>{elem.Title}</div>
+          </div>
+          )
+      )}
+      </div>
     </div>
-  )
+  );
 }
